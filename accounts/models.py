343b21c76be02e7146187b7c9919b882c916e34b
Adrienri <adriiianperez@fpuna.edu.py>
2024-10-30 19:14:40 -0300

    Refactorizar el modelo User y Blog para utilizar CloudinaryField en lugar de ImageField

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
 from django.utils.translation import gettext as _
 from .managers import UserManager
+from cloudinary.models import CloudinaryField
 
 class User(AbstractUser):
     """
@@ -37,7 +38,7 @@ class User(AbstractUser):
     
     is_author = models.BooleanField(default=False)
 
-    avatar = models.ImageField(upload_to="avatars/", default="avatars/default.jpeg")
+    avatar = CloudinaryField('image', default="avatars/default.jpeg")
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
