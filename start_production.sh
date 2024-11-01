9cee2f3f579843dae76a6ffa7897264ac73e87b9
Adrienri <adriiianperez@fpuna.edu.py>
2024-11-01 10:24:13 -0300

    Fix script producción

--- start_production.sh
+++ start_production.sh
@@ -5,11 +5,11 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+# Iniciar Gunicorn en segundo plano con 1 trabajador
+gunicorn cloudcms.wsgi --log-file - --workers=1 &
+
 # Ejecutar el worker de Celery en segundo plano
 nohup celery -A cloudcms worker --loglevel=info --concurrency=1
 
 # Ejecutar el beat scheduler de Celery en segundo plano
 nohup celery -A cloudcms beat --loglevel=info &
-
-# Iniciar Gunicorn en segundo plano con 1 trabajador
-gunicorn cloudcms.wsgi --log-file - --workers=1 &
